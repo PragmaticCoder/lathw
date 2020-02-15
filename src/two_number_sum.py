bf276f151a728ee525de1651f3ae670becd259b1
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-02-15 13:05:32 -0600

    added question for two number sum and initial setup

---
+++ src/two_number_sum.py
@@ -0,0 +1,22 @@
+"""
+Write a function that takes in a non-empty list of distinct integers and an integer representing a target sum
+as arguments.
+
+If any two numbers in the input array sum up to the target sum,
+the function should return them as tuples.
+
+If no two numbers adds to target sum, the function should return None.
+If there are more than two numbers, return any two numbers pair.
+"""
+
+
+def two_number_sum(nums, target_sum):
+    pass
+
+
+def main():
+    print(two_number_sum([1, 22, 4, 5, 7], 6))
+
+
+if __name__ == '__main__':
+    main()
